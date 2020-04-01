e269e6bdac23636d6f3482be6a2b377e4fec8735
github username <github email address>
2020-04-01 14:06:34 -0400

    changes added

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,31 +1,25 @@
 const fs = require('fs') ;
 const http = require('http') ; 
 const url = require('url') ; 
+const slugify = require('slugify')  ; 
+const replaceTemplate = require('./modules/replaceTemplate')
 
 
 ///////////////////////////////
 // SERVER 
 ////////////////////////////// 
 
-const replaceTemplate = (temp,product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName) ; 
-    output = output.replace(/{%IMAGE%}/g,product.image) ;
-    output = output.replace(/{%PRICE%}/g,product.price) ;
-    output = output.replace(/{%NUTRIENTS%}/g,product.nutrients) ;
-    output = output.replace(/{%QUANTITY%}/g,product.quantity) ;
-    output = output.replace(/{%DESCRIPTION%}/g,product.description) ;
-    output = output.replace(/{%FROM%}/g,product.from) ;
-    output = output.replace(/{%ID%}/g,product.id) ;
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic') ;
-    return output  ;
-}
+
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8') ;
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8') ; 
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8') ;
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8') ;
 const dataObj = JSON.parse(data);
+const slugs = dataObj.map(el => slugify(el.productName , {lower : true})) ; 
 
+// log slugs 
+console.log(slugs) ; 
 
 const server = http.createServer((req,res) => {
 
@@ -49,6 +43,7 @@ const server = http.createServer((req,res) => {
     }  else if (pathname === '/api') {
         res.writeHead(200,{'Content-type':'application/json'})
         res.end(data) ; 
+
 
     // Not Found 
     } else {
