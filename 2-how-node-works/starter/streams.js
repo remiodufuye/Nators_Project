c68991b14ab91765b778e7055c3fc42e7b93021f
github username <github email address>
2020-04-04 11:29:28 -0400

    changes made

--- 2-how-node-works/starter/streams.js
+++ 2-how-node-works/starter/streams.js
@@ -7,19 +7,24 @@ server.on('request', (req, res) => {
   //     if (err) console.log(err);
   //     res.end(data);
   //   });
+
   // Solution2
+  //   const readable = fs.createReadStream('test-file.txt');
+  //   readable.on('data', chunk => {
+  //     res.write(chunk);
+  //   });
+  //   readable.on('end', () => {
+  //     res.end();
+  //   });
+  //   readable.on('error', err => {
+  //     console.log(err);
+  //     res.statusCode = 500;
+  //     res.end('File not Found');
+  //   });
+
+  // Solution 3
   const readable = fs.createReadStream('test-file.txt');
-  readable.on('data', chunk => {
-    res.write(chunk);
-  });
-  readable.on('end', () => {
-    res.end();
-  });
-  readable.on('error', err => {
-    console.log(err);
-    res.statusCode = 500;
-    res.end('File not Found');
-  });
+  readable.pipe(res);
 });
 
 server.listen(8000, '127.0.0.1', () => {
