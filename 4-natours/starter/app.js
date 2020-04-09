43a6320285f4b7e800744a059e09b83df604280f
github username <github email address>
2020-04-09 13:04:04 -0400

    changes made

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello From The Server Side!', app: 'Natours' });
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello From The Server Side!', app: 'Natours' });
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      // tours: tours ... can be re-eritten as below
+      tours,
+    },
+  });
 });
 
 app.post('/', (req, res) => {
