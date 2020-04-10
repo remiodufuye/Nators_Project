4b39d79bb26b7a993412b18c446690059c5a37a6
github username <github email address>
2020-04-10 07:03:14 -0400

    changes made

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -13,14 +13,23 @@ app.get('/api/v1/tours', (req, res) => {
     status: 'success',
     results: tours.length,
     data: {
-      // tours: tours ... can be re-eritten as below
       tours,
     },
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  // if (id > tours.length)
+  if (!tour) {
+    return res.status(404).json({ status: 'failed', message: 'Invalid ID' });
+  }
+  res.status(200).json({ status: 'success', data: { tour } });
+});
+
 app.post('/api/v1/tours', (req, res) => {
-  // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
@@ -31,6 +40,15 @@ app.post('/api/v1/tours', (req, res) => {
       res.status(201).json({ status: 'success', data: { tour: newTour } });
     }
   );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({ status: 'failed', message: 'Invalid ID' });
+  }
+  res
+    .status(200)
+    .json({ status: 'success', data: { tour: '< Updated Tour Here !! >' } });
 });
 
 const port = 3000;
