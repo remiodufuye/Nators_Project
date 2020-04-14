2f564601faa4d4877be6ff9b66dcc0c62e3a898b
github username <github email address>
2020-04-14 19:38:28 -0400

    changes made

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,57 +1,39 @@
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour ID is :${val}`);
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({ status: 'failed', message: 'Invalid ID' });
-  }
-  next();
-};
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res
-      .status(400)
-      .json({ status: 'fail', message: 'Missing Name or Price!!' });
-  }
-  next();
-};
+const Tour = require('./../models/tourModels');
 
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
     requestedAT: req.requestTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
   });
 };
 
 exports.getTour = (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
+  // const tour = tours.find((el) => el.id === id);
 
-  res.status(200).json({ status: 'success', data: { tour } });
+  // res.status(200).json({ status: 'success', data: { tour } });
 };
 
-exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({ status: 'success', data: { tour: newTour } });
-    }
-  );
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: { tour: newTour },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'failed',
+      message: 'Invalid Data Sent!!',
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-// console.log(process.env);
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  // .connect(process.env.DATABASE_LOCAL, { /*  Connects to Local Database  */
+  .connect(DB, {
+    /*  Connects to Hosted Database on MongoDB Atlas   */
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB Connection Succesful'));
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
