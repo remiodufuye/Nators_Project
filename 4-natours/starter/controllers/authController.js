6cbcc98ebf51236804a02a1a48b6e401c0949664
github username <github email address>
2020-04-27 15:13:43 -0400

    changes made

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -1,13 +1,75 @@
+const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  // const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
   });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  // 1) Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please Provide email and password!', 400));
+  }
+  // 2) Check if User exists and password is correct
+  const user = await User.findOne({
+    email,
+  }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or Password!', 401));
+  }
+
+  // 3) If Everything is Okay send Token to Client
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1.) Getting Token and Check if its there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  console.log(token);
+
+  if (!token) {
+    return next(
+      new AppError('You are not logged In! Please log in to get access.', 401)
+    );
+  }
+  // 2.) Verification Token
+  // 3.) Check if User Still Exists
+  // 4.) Check if USer changed password after the Token was issued .
+
+  next();
 });
