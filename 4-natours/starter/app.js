2fec111ca18f7615a12603886c746bd27d5f8890
github username <github email address>
2020-05-12 16:22:55 -0400

    changes made

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -11,6 +11,7 @@ const globalErrorHandler = require('./controllers/errorControllers');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 const app = express();
 
 // 1.) GLOBAL MIDDLE WARES
@@ -72,6 +73,7 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
