295e954de545eee6b943bd150f2c550828079659
github username <github email address>
2020-06-04 03:10:28 -0400

    changes made

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -174,20 +174,13 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   // 2.) Generate Random Reset Token
   const resetToken = user.createPasswordResetToken();
   await user.save({ validateBeforeSave: false });
+
   // 3.) Send it to user's email
-  const resetURL = `${req.protocol}://${req.get(
-    'host'
-  )}/api/v1/users/resetPassword/${resetToken}`;
-  const message = `Forgot Your Password ? Submmit a PATCH request with your new password and passwwordConfirm to : ${resetURL} .\n 
-  If You Did not forget your password please ignore this email!`;
-
   try {
-    // await sendEmail({
-    //   email: user.email,
-    //   subject: 'Your Password Reset Token(Valid for 10 mins )',
-    //   message,
-    // });
-
+    const resetURL = `${req.protocol}://${req.get(
+      'host'
+    )}/api/v1/users/resetPassword/${resetToken}`;
+    await new Email(user, resetURL).sendPasswordReset();
     res.status(200).json({
       status: 'success',
       message: 'Token Sent to Email!',
