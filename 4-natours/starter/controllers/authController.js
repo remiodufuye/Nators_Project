bb0e50e208c7ce4360751d9bb568bc8d3a8fd2d9
github username <github email address>
2020-06-04 02:23:40 -0400

    changes made

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
-const sendEmail = require('./../utils/email');
+const Email = require('./../utils/email');
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -38,13 +38,10 @@ const createSendToken = (user, statusCode, res) => {
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
+  const url = `${req.protocol}://${req.get('host')}/me`;
+  console.log(url);
+  await new Email(newUser, url).sendWelcome();
   createSendToken(newUser, 201, res);
-  // const newUser = await User.create({
-  //   name: req.body.name,
-  //   email: req.body.email,
-  //   password: req.body.password,
-  //   passwordConfirm: req.body.passwordConfirm,
-  // });
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -185,11 +182,11 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   If You Did not forget your password please ignore this email!`;
 
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your Password Reset Token(Valid for 10 mins )',
-      message,
-    });
+    // await sendEmail({
+    //   email: user.email,
+    //   subject: 'Your Password Reset Token(Valid for 10 mins )',
+    //   message,
+    // });
 
     res.status(200).json({
       status: 'success',
--- 4-natours/starter/public/js/bundle.js
+++ 4-natours/starter/public/js/bundle.js
@@ -8875,12 +8875,12 @@ if (loginForm) loginForm.addEventListener('submit', function (e) {
 if (logOutbtn) logOutbtn.addEventListener('click', _login.logout);
 if (userDataForm) userDataForm.addEventListener('submit', function (e) {
   e.preventDefault();
-  var name = document.getElementById('name').value;
-  var email = document.getElementById('email').value;
-  (0, _updateSettings.updateSettings)({
-    name: name,
-    email: email
-  }, 'data');
+  var form = new FormData();
+  form.append('name', document.getElementById('name').value);
+  form.append('email', document.getElementById('email').value);
+  form.append('photo', document.getElementById('photo').files[0]);
+  console.log(form);
+  (0, _updateSettings.updateSettings)(form, 'data');
 });
 if (userPasswordForm) userPasswordForm.addEventListener('submit', /*#__PURE__*/function () {
   var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(e) {
@@ -8947,7 +8947,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50323" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61960" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
